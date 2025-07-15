4e07e8cd31e2862c6f427540a5a74c4a87d3010f
Irina Nazarova <inazarova@evilmartians.com>
2025-07-14 23:02:57 -0700

    Fix esbuild Unexpected const error

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ export default defineConfig({
     inlineStylesheets: 'auto',
   },
   vite: {
+    optimizeDeps: {
+      include: ['animejs']
+    },
     build: {
       cssCodeSplit: true,
       rollupOptions: {
